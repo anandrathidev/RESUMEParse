82b4122daea7414c00d70d1a37b2dfac0cd75a6a
AnandDev <anandrathi.dev@gmail.com>
2018-05-16 23:12:25 +0800

    Update ResumeParaAnalizer.py

--- ResumeParaAnalizer.py
+++ ResumeParaAnalizer.py
@@ -4,34 +4,14 @@ Created on Sun May 13 22:14:40 2018
 
 @author: anandrathi
 """
-
-from nltk.corpus import wordnet
-import nltk
-nltk.download('maxent_ne_chunker')
-nltk.download('words')
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('stopwords')
-
 import pandas as pd
 import re
 import numpy as np
 
 
-#import enchant
-from nltk.corpus import stopwords
-STOP_WORDS=set(stopwords.words('english'))
-
 xpath = "C:/Users/anand.rathi/Documents/tmp/ALEEP/"
 xpath = "D:/Users/anandrathi/Documents/personal/Bussiness/Aleep/"
 
-HEADINGS=set()
-with open(xpath+"/Headings.txt","r") as HeadFile:
-  for line in HeadFile.readlines():
-    line=line.replace(":","").strip()
-    HEADINGS.add(line)
-
-HEADINGS=list(HEADINGS)
 
 EMAIL_PATTERN = r'[\w\.-]+@[\w\.-]+'
 NO_NAME = set(["phone","resume", "vitae" , "of", "curriculum", "curriculam"," carriculum", "curriculum-vitae",
@@ -52,6 +32,7 @@ PAT = re.compile(EMAIL_PATTERN)
 resdfInit = pd.read_json(xpath + "ALLRES.json", orient='records')
 list(resdfInit.columns)
 resdfInit.head(1).index
+
 res1 = resdfInit['RESUME_TEXT'].head(10000)[202]
 print(res1)
 
@@ -68,16 +49,19 @@ def GetHeadingsSets():
 HeadSet = GetHeadingsSets()
 type(HeadSet)
 
+HeadSet
+
 "project" in HeadSet
 
 avgHeaderLen = np.mean([ len(head) for  head in HeadSet if len(head.strip())>0 ])
+stdHeaderLen = np.std([ len(head) for  head in HeadSet if len(head.strip())>0 ])
+
 avgHeaderWords = np.mean([ len(head.split()) for  head in HeadSet if len(head.strip())>0 ])
 stdHeaderWords = np.std([ len(head.split()) for  head in HeadSet if len(head.strip())>0 ])
 
 def CreateAllHeadRegExp(HeadSet):
   regList = []
   for h in HeadSet:
-    HeadExtraxtRegStr = h
     HeadExtraxtRe = re.compile(h, flags= re.I)
     regList.append(HeadExtraxtRe)
   return regList
@@ -97,21 +81,27 @@ def CreateHeadRegExp(HeadSet):
 
 hregall =  CreateAllHeadRegExp(HeadSet)
 
-avgLen = np.mean([ len(rline) for  rline in rlines if len(rline.strip())>0 ])
-avgWordCount = np.mean([ len(rline.split()) for  rline in rlines if len(rline.strip())>0 ])
-stdWordCount = np.std([ len(rline.split()) for  rline in rlines if len(rline.strip())>0 ])
 
 from scipy import stats
 np.random.seed(7654567)
 
-rvsWord = stats.norm.rvs(loc=avgWordCount, scale=stdWordCount, size=(50))
-rvsHeader = stats.norm.rvs(loc=avgHeaderWords, scale=stdHeaderWords, size=(50))
 
 HeaderStack=[]
 Para=[]
 ParaLines=[]
 rlines = res1.splitlines()
 lastHeader=""
+
+avgWordLen = np.mean([ len(rline) for  rline in rlines if len(rline.strip())>0 ])
+stdWordLen = np.std([ len(rline) for  rline in rlines if len(rline.strip())>0 ])
+avgWordCount = np.mean([ len(rline.split()) for  rline in rlines if len(rline.strip())>0 ])
+stdWordCount = np.std([ len(rline.split()) for  rline in rlines if len(rline.strip())>0 ])
+
+rvsWord = stats.norm.rvs(loc=avgWordCount, scale=stdWordCount, size=(50))
+rvsHeader = stats.norm.rvs(loc=avgHeaderWords, scale=stdHeaderWords, size=(50))
+rvsWordLen = stats.norm.rvs(loc=avgWordLen, scale=stdWordLen, size=(50))
+rvsHeaderLen = stats.norm.rvs(loc=avgHeaderLen, scale=stdHeaderLen, size=(50))
+
 for line in rlines:
    isHeader=0
    isMatch=0
@@ -123,14 +113,27 @@ for line in rlines:
      #most probablity this is an header line
      isHeader+=1
      isMatch=1
-     print("is in header list :{}".format(line))
+     print("is in header list :{}   {}".format(line, isHeader))
    pval = stats.ttest_1samp(rvsWord, wcnt)
    if abs(pval.pvalue) < 0.05:
      isHeader+=1
+     print("This is header rvsWord test :{} {}".format(isHeader, abs(pval.pvalue)))
    wpval = stats.ttest_1samp(rvsHeader, wcnt)
    if abs(wpval.pvalue) > 0.05:
      isHeader+=1
-   if isHeader>2 and isMatch:
+     print("This is header rvsHeader test :{} {}".format(isHeader, abs(wpval.pvalue)))
+
+   pval = stats.ttest_1samp(rvsWordLen, len(line))
+   if abs(pval.pvalue) < 0.05:
+     isHeader+=1
+     print("This is header rvsWordLen test :{} {}".format(isHeader, abs(pval.pvalue)))
+   wpval = stats.ttest_1samp(rvsHeaderLen, len(line))
+   if abs(wpval.pvalue) > 0.05:
+     isHeader+=1
+     print("This is header rvsHeaderLen test :{} {}".format(isHeader, abs(wpval.pvalue)))
+
+   if isHeader>=3 and isMatch:
+     print("This is header :{}".format(line))
      if len(HeaderStack) >0:
        lastHeader=HeaderStack.pop()
      if len(ParaLines) >0:
@@ -161,80 +164,5 @@ if len(ParaLines) >0:
   lastHeader=""
   ParaLines=[]
 
-print(Para)
-
-#ParaReg = re.compile(r"(\n|^)(.*?)(?=\n|$)")
-
-ParaReg = re.compile(r"(\.\s)?(?[A-Z][^\.]+\.)")
-i=0
-for paras in re.finditer(ParaReg, res1):
-  i=i+1
-  print(i)
-  if not paras is None:
-    print("============== START")
-    if not paras.group(0) == "":
-      print("============== group 0")
-      print( paras.group(0))
-    if not paras.group(1) == "":
-      print("============== group 1")
-      print( paras.group(1))
-    #if not paras.group(2) == "":
-    #  print("============== group 2")
-    #  print( paras.group(2))
-    print("============== END")
-
-print(matchRes.group(0))
-print(matchRes.group(1))
-print(matchRes.group(2))
-print(matchRes.group(3))
-print(matchRes.group(4))
-print(matchRes.group(5))
-
-
-HeadExtraxtRe = CreateHeadRegExp(list(HeadSet))
-matchRes =  HeadExtraxtRe.search(res1)
-
-for rline in res1.split("\n"):
-  if
-
-print(matchRes.group(0))
-print(matchRes.group(1))
-print(matchRes.group(2))
-print(matchRes.group(3))
-print(matchRes.group(4))
-print(matchRes.group(5))
-
-
-
-def ExtraxtParas(HeadExtraxtRe):
-  HeadExtraxtRegStr = "(" + "|".join(HeadSet) +   ")" + "(.*?)()" + "(" + "|".join(HeadSet) +   ")"
-  HeadExtraxtRe = re.compile(HeadExtraxtRegStr, flags= re.I | re.DOTALL )
-  return HeadExtraxtRe
-
-
-headings=[]
-def ExtractHeading(x):
-  #Parainfo={}
-  paraList = x.lstrip().split("\n\n")
-  total = sum([len(para) for para in  paraList])
-  avgwords = np.average([len(para.split()) for para in  paraList])
-  headingsSet = set(headings)
-  headingsSet.update(set([ para for para in  paraList  if len(para.split()) < 4 ]))
-  headings.clear()
-  headings.extend(list(headingsSet))
-
-def getHeadingSet():
-  nonedf = pd.DataFrame(resdfInit.apply(lambda x: ExtractHeading(x=x['RESUME_TEXT']) , axis=1))
-  Heading = HeadingDFJson['HEADINGS'].tolist()
-  HeadingTxt = ",".join(Heading)
-  HeadingSet = set([  h.strip() for h in HeadingTxt.split(",")])
-  return (HeadingSet)
-
-
-from collections import Counter
-words = []
-for morewords in firstLineDF['Firstline'].str.split() :
-  #print(type(morewords))
-  words.extend(morewords)
-wordcount = Counter(words)
+#print(Para)
 
